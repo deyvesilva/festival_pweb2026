207c7770de1249460f4c449f90f11c2790f23ffe
deyvesilva <deyvedasilva12@gmail.com>
2026-04-23 15:56:04 +0000

    Correção do site

--- festival/urls.py
+++ festival/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
+    path('dias/', views.dias_view, name='dias'),
     path('palcos/', views.palcos_view, name='palcos'),
-    path('concertos/<int:id>/', views.concerto_view, name='concerto'),
+    path('dia/<int:dia_id>/', views.dia_view, name='dia'),
+    path('palco/<int:palco_id>/', views.palco_view, name='palco'),
+    path('concerto/<int:concerto_id>/', views.concerto_view, name='concerto'),
 ]
--- festival/views.py
+++ festival/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
-from .models import Palco
-
+from .models import Palco, Dia, Banda, Concerto
 
 def index_view(request):
     return render(request, 'festival/index.html')
 
-
 def dias_view(request):
     dias = Dia.objects.all() 
-
     context = {'dias': dias}
-
     return render(request, 'festival/dias.html', context)
 
+def palcos_view(request):
+    palcos = Palco.objects.all()
+    context = {'palcos': palcos}
+    return render(request, 'festival/palcos.html', context)
 
+def dia_view(request, dia_id):
+    dia = Dia.objects.get(id=dia_id)
+    # Filtra os concertos apenas deste dia
+    concertos = Concerto.objects.filter(dia=dia)
+    context = {'dia': dia, 'concertos': concertos}
+    return render(request, 'festival/dia.html', context)
 
-def concerto_view(request, id):
-    concerto = 
+def palco_view(request, palco_id):
+    palco = Palco.objects.get(id=palco_id)
+    # Filtra os concertos apenas deste palco
+    concertos = Concerto.objects.filter(palco=palco)
+    context = {'palco': palco, 'concertos': concertos}
+    return render(request, 'festival/palco.html', context)
 
+def concerto_view(request, concerto_id):
+    # Vai buscar o concerto específico pelo ID
+    concerto = Concerto.objects.get(id=concerto_id)
     context = {'concerto': concerto}
-
     return render(request, 'festival/concerto.html', context)
